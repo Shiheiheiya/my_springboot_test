148a49c1c8cb0e9300eaf2bf917eb009c4f3e337
li <1849588907@qq.com>
2021-01-22 10:24:03 +0800

    Initial commit

---
+++ src/main/java/com/mytest/my_springboot_test/MySpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.mytest.my_springboot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootTestApplication.class, args);
+    }
+
+}
